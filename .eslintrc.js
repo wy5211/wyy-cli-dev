a3a29a6339cd8a6f3f2394b3522f00b85225359f
王阳 <wangyang@jiaogepengyou.com.cn>
2022-05-10 16:30:15 +0800

    chore: add lint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'global-require': 0,
+    'no-unused-vars': 1,
+    'comma-dangle': 0,
+    'implicit-arrow-linebreak': 0,
+  },
+};
